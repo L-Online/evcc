03d0ef1f8f2fbb912f08c6bab7cad2456bc4fc63
L-Online <luke@nurfuerspam.de>
2020-12-02 23:04:20 +0100

    created separate file for my version (phoenix-ludwig) of the phoenix-evcc

--- charger/phoenix-evcc.go
+++ charger/phoenix-evcc.go
@@ -9,10 +9,7 @@ import (
 )
 
 const (
-	phEVCCRegEnConfig   = 4000  // Holding, Remanent
-	phEVCCRegOutConfig  = 5500  // Holding, Remanent
 	phEVCCRegEnable     = 20000 // Coil
-	phEVCCRegOUT        = 23000 // Holding
 	phEVCCRegMaxCurrent = 22000 // Holding
 	phEVCCRegStatus     = 24000 // Input
 )
@@ -20,16 +17,12 @@ const (
 // PhoenixEVCC is an api.ChargeController implementation for Phoenix EV-CC-AC1-M wallboxes.
 // It uses Modbus TCP to communicate with the wallbox at modbus client id 255.
 type PhoenixEVCC struct {
-	log    *util.Logger
-	conn   *modbus.Connection
-	enMode string
+	conn *modbus.Connection
 }
 
 func init() {
 	registry.Add("phoenix-evcc", NewPhoenixEVCCFromConfig)
 }
-
-//go:generate go run ../cmd/tools/decorate.go -p charger -f decoratePhoenixEVCC -b api.Charger -o phoenix-evcc_decorators -t "api.Meter,CurrentPower,func() (float64, error)" -t "api.MeterEnergy,TotalEnergy,func() (float64, error)" -t "api.MeterCurrent,Currents,func() (float64, float64, float64, error)"
 
 // NewPhoenixEVCCFromConfig creates a Phoenix charger from generic config
 func NewPhoenixEVCCFromConfig(other map[string]interface{}) (api.Charger, error) {
@@ -54,46 +47,13 @@ func NewPhoenixEVCC(uri, device, comset string, baudrate int, id uint8) (*Phoeni
 	if err != nil {
 		return nil, err
 	}
-	var enMode string
-	//	enMode = "modbus"
+
 	log := util.NewLogger("evcc")
 	conn.Logger(log.TRACE)
 
 	wb := &PhoenixEVCC{
-		log:    log,
-		conn:   conn,
-		enMode: enMode,
+		conn: conn,
 	}
-
-//	wb.conn.WriteSingleRegister(phEVCCRegEnConfig, 1)  //write to registers for debugging
-//	wb.conn.WriteSingleRegister(phEVCCRegOutConfig, 0) //write to registers for debugging
-
-	RegEnConfig, err := wb.conn.ReadInputRegisters(phEVCCRegEnConfig, 1)
-	if err != nil {
-		return nil, err
-	}
-	wb.log.TRACE.Printf("Register 4000 is %d", RegEnConfig[1])
-
-	RegOutConfig, err := wb.conn.ReadInputRegisters(phEVCCRegOutConfig, 1)
-	if err != nil {
-		return nil, err
-	}
-	wb.log.TRACE.Printf("Register 5500 is %d", RegOutConfig[1])
-
-	if RegEnConfig[1] == 1 && RegOutConfig[1] == 0 {
-
-		enMode = "out-pin"
-		wb.log.TRACE.Printf("enMode: %s; Charger is enabled by switching the digital OUT on.", enMode)
-
-	} else if RegEnConfig[1] == 3 {
-		enMode = "modbus"
-		wb.log.TRACE.Printf("enMode: %s; Charger is enabled by modbus.", enMode)
-
-	} else {
-		wb.log.ERROR.Printf("Registers 4000 = %d (and 5500 = %d) of Phoenix evcc are not configured for remote enabling", RegEnConfig[1], RegOutConfig[1])
-	}
-
-	wb.enMode = enMode
 
 	return wb, nil
 }
@@ -110,57 +70,22 @@ func (wb *PhoenixEVCC) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the Charger.Enabled interface
 func (wb *PhoenixEVCC) Enabled() (bool, error) {
-	var EN bool
-	wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
-
-	d, err := wb.conn.ReadInputRegisters(phEVCCRegOUT, 1)
-	if err != nil {
-		return false, err
-	}
-	wb.log.TRACE.Printf("Register 23000 is %d)", d)
-
 	b, err := wb.conn.ReadCoils(phEVCCRegEnable, 1)
 	if err != nil {
 		return false, err
 	}
-	wb.log.TRACE.Printf("Register 20000 is %d)", b)
 
-	if wb.enMode == "out-pin" {
-		EN = d[1] == 1
-	} else {
-		EN = b[0] == 1
-	}
-	wb.log.TRACE.Printf("enabled: %t", EN)
-
-	return EN, nil
+	return b[0] == 1, nil
 }
 
 // Enable implements the Charger.Enable interface
 func (wb *PhoenixEVCC) Enable(enable bool) error {
 	var u uint16
-	wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
-
-	wb.log.TRACE.Printf("enable: %t;", enable)
-
 	if enable {
 		u = 0xFF00
 	}
+
 	_, err := wb.conn.WriteSingleCoil(phEVCCRegEnable, u)
-	if err != nil {
-		return err
-	}
-
-	if wb.enMode == "out-pin" {
-		if enable {
-			u = 0x0001
-		}
-		//High-signal on pin OUT of the EV_CC_AC1-M  board (wire bridge between OUT and ENABLE necessary!!)
-		_, err := wb.conn.WriteSingleRegister(phEVCCRegOUT, u)
-		if err != nil {
-			return err
-		}
-
-	}
 
 	return err
 }
